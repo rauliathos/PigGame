26ea723399a4de316e62f799dfe9db5f02d4a643
Raul Ciobanu <rauliathos@gmail.com>
2020-12-09 14:44:13 +0000

    implemented NEW GAME button logic

--- script.js
+++ script.js
@@ -13,14 +13,27 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+let scores, currentScore, activePlayer, playing;
 // Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+
+const init = function() {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+init();
 const switchPlayer = function() {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   currentScore = 0;
@@ -65,7 +78,7 @@ btnHold.addEventListener('click', function() {
       scores[activePlayer];
 
     // 2. Check if player score is >= 100
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       // Finsih the game
       playing = false;
       diceEl.classList.add('hidden');
@@ -78,3 +91,5 @@ btnHold.addEventListener('click', function() {
     }
   }
 });
+
+btnNew.addEventListener('click', init);
